596887bed301ac5b4a0f784f63e2fbf14d288205
“Andrew <abiv23@gmail.com>
2016-11-16 09:52:04 -0700

    api plugged in and taking user input steps 1 and 2 complete

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,12 +4,10 @@ $(document).ready(function() {
 
 function submitButtonClicked(event) {
     event.preventDefault();
-    console.log('click handler working!');
     var name = $('.team').val()
     var Team = $('.teamText1')
     Team.append('<div class="col m6"><div class="card blue-grey"><div class="card-content white-text"><span class="card-title scoreBoard1"><p>' + $('.team').val().toUpperCase() + ' Team Crimes: </p><span>');
     $.get("http://nflarrest.com/api/v1/team/search/?term=" + name, useLookUp)
-    console.log('this was run the first time');
 }
 
 function useLookUp(data) {
@@ -25,32 +23,45 @@ function displayPlayers(data) {
     var playerText = $('.outputPlayer')
     playerText.append('<div class="col m4"></div><h5 class="col m5">Player Arrest Details: </h5></div>');
     for (var i = 0; i < data.length; i++) {
-        var Name = ('<div class="col m5"><div class="card blue-grey"><div class="card-content white-text"><span class="card-title">' + data[i].Name + '</span><p class="player_name">Date: ' + data[i].Date + '</p><p>Charge: ' + data[i].Category + '</p><p>Details: ' + data[i].Description + '</p></div></div></div></div>')
+        var Name = ('<div class="col m5"><div class="card blue-grey"><div class="card-content white-text"><span class="card-title">' + data[i].Name + '</span><p class="player_name">Date: ' + data[i].Date + '</p><p>Charge: ' + data[i].Category + '</p><p class="truncate details">Details: ' + data[i].Description + '</p></div></div></div></div>')
         playerText.append(Name);
     }
     $('.hero-label').text('Compare to your Team');
     $('.team').attr("placeholder", "Enter your Favorite Team to see the Moral Scoreboard");
+    $('.truncate').on('click', truncateContent)
+}
+
+function truncateContent() {
+    $('.truncate').removeClass('truncate');
+    $('.truncate').off('click', truncateContent);
+    $('.details').on('click', addTruncate);
+}
+
+function addTruncate() {
+    $('.details').addClass('truncate');
+    $('.details').off('click', addTruncate);
+    $('.details').on('click', truncateContent);
 }
 
 function displayTeam(data) {
     for (var i = 0; i < data.length; i++) {
-      var teamCrimes = ('<p>' + data[i].Category + ': ' + data[i].arrest_count + '</p>')
-      $('.scoreBoard1').append(teamCrimes);
+        var teamCrimes = ('<p>' + data[i].Category + ': ' + data[i].arrest_count + '</p>')
+        $('.scoreBoard1').append(teamCrimes);
     }
 }
 
 function compareClicked() {
-  event.preventDefault();
-  console.log('second click listener working!');
-  var name = $('.team').val();
-  var Team = $('.teamText1')
-  Team.append('<div class="col m6"><div class="card blue-grey"><div class="card-content white-text"><span class="card-title scoreBoard1"><p>' + $('.team').val().toUpperCase() + ' Team Crimes: </p><span>');
-  $.get('http://nflarrest.com/api/v1/team/search/?term=' + name, useLookUp);
-  $('button').off('click', compareClicked);
-  $('button').html('Reset');
-  $('button').on('click', function(){
-  $('main').reset();
-  })
+    event.preventDefault();
+    console.log('second click listener working!');
+    var name = $('.team').val();
+    var Team = $('.teamText1')
+    Team.append('<div class="col m6"><div class="card blue-grey"><div class="card-content white-text"><span class="card-title scoreBoard1"><p>' + $('.team').val().toUpperCase() + ' Team Crimes: </p><span>');
+    $.get('http://nflarrest.com/api/v1/team/search/?term=' + name, useLookUp);
+    $('button').off('click', compareClicked);
+    $('button').html('Reset');
+    $('button').on('click', function() {
+        $('main').reset();
+    })
 }
 
 function useLookUp2(data) {
@@ -59,9 +70,9 @@ function useLookUp2(data) {
 }
 
 function displayTeam2(data) {
-  for (var i = 0; i < data.length; i++){
-    var teamCrimes = ('<p>' + data[i].Category + ': ' + data[i].arrest_count + '</p>')
-    $('.scoreBoard2').append(teamCrimes);
-  }
-  $('.hero-label').text('The Moral Scoreboard');
+    for (var i = 0; i < data.length; i++) {
+        var teamCrimes = ('<p>' + data[i].Category + ': ' + data[i].arrest_count + '</p>')
+        $('.scoreBoard2').append(teamCrimes);
+    }
+    $('.hero-label').text('The Moral Scoreboard');
 }
